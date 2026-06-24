35072e99fdd9606e1ead0023d8e89082f236616f
vivekpalhal <vivekpalhal1@gmail.com>
2026-06-24 18:55:47 +0530

    addition using input form user

--- firstjs.js
+++ firstjs.js
@@ -1,5 +1,11 @@
-console.log("this is my first time git practice")
-// addition of 2 numbers in js
-let a = 5;
-let b = 20;
-console.log("a+b = ", a+b);
+// console.log("this is my first time git practice")
+// // addition of 2 numbers in js
+// let a = 5;
+// let b = 20;
+// console.log("a+b = ", a+b);
+
+// addition of two no using input
+let a = Number(prompt("enter your first no: "));
+let b = Number(prompt("enter your second no: "));
+let c = a + b;
+console.log("addition of a+b = ", c);
