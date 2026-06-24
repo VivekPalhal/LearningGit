e1209116bfea8496978a781891490b3eec4586fe
vivekpalhal <vivekpalhal1@gmail.com>
2026-06-24 19:01:25 +0530

    a*b done

--- firstjs.js
+++ firstjs.js
@@ -5,7 +5,14 @@
 // console.log("a+b = ", a+b);
 
 // addition of two no using input
-let a = Number(prompt("enter your first no: "));
-let b = Number(prompt("enter your second no: "));
-let c = a + b;
-console.log("addition of a+b = ", c);
+// let a = Number(prompt("enter your first no: "));
+// let b = Number(prompt("enter your second no: "));
+// let c = a + b;
+// console.log("addition of a+b = ", c);
+
+//multipication of two no 
+ 
+let a = Number(prompt("enter a = "));
+let b = Number(prompt("enter b = "));
+let c = a * b;
+console.log("a*b = ", c);
