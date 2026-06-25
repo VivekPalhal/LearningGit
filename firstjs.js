53e1ade548a072e7766cd4fa589c060d0b37510a
vivekpalhal <vivekpalhal1@gmail.com>
2026-06-25 23:12:22 +0530

    again added removed data

---
+++ firstjs.js
@@ -0,0 +1,27 @@
+// console.log("this is my first time git practice")
+// // addition of 2 numbers in js
+// let a = 5;
+// let b = 20;
+// console.log("a+b = ", a+b);
+
+// addition of two no using input
+// let a = Number(prompt("enter your first no: "));
+// let b = Number(prompt("enter your second no: "));
+// let c = a + b;
+// console.log("addition of a+b = ", c);
+
+//multipication of two no 
+// let a = Number(prompt("enter a = "));
+// let b = Number(prompt("enter b = "));
+// let c = a * b;
+// console.log("a*b = ", c);
+
+//print 1 to 10 no
+// for ( i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// // printing vivekpalhal infinte times 
+// for (i=0; i>1; i++){
+//     console.log("vivekpalhal");
+// }
